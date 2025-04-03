de6b6ec3a491986e764b60ae1022eb9c0685c0d6
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-04-03 10:09:39 -0300

    testando a IDE

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+# print("Hello World")
+
+# Função para exibir o nome
+
+def exibir_nome():
+    nome = input("Digite o seu nome: ")
+    print(nome)
+
+# Módulo Principal
+if __name__ == '__main__':
+    exibir_nome()
